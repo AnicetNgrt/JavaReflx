2c16131b11c899e93a65fbb5741d77b93ddb2524
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2021-02-24 11:58:30 +0100

    launched prog serv

---
+++ src/main/java/javareflx/bri/prog/ServiceProg.java
@@ -0,0 +1,40 @@
+package javareflx.bri.prog;
+
+import javareflx.bri.exceptions.InstanceNotFoundException;
+import javareflx.bri.services.Service;
+import javareflx.bri.services.ServiceRegistry;
+import javareflx.bri.services.Session;
+
+import java.net.Socket;
+
+class ServiceProg extends Service {
+
+	ServiceProg(Socket socket) {
+		super(socket, new Session());
+	}
+
+	@Override
+	protected void onClientMessage(String message) {
+		int choix = Integer.parseInt(message);
+		try {
+			Class<?> serviceClass = ServiceRegistry.getServiceClass(choix);
+			try {
+				Service service = (Service) serviceClass.newInstance();
+				service.init(getSocket(), getSession());
+				new Thread(service).start();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}catch (InstanceNotFoundException e){
+			sendMessage(e.getMessage() + "\n" + e);
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		sendMessage(ServiceRegistry.staticToString()+"\n##Tapez le numéro de service désiré :");
+		receive();
+	}
+}
