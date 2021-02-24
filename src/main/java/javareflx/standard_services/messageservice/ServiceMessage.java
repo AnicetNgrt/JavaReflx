eec01b71e4d32bb690851fe111219555738b0e1d
ThibaultHenrion <60553782+ThibaultHenrion@users.noreply.github.com>
2021-02-24 19:13:13 +0100

    optimize MessageServices storage

--- src/main/java/javareflx/standard_services/messageservice/ServiceMessage.java
+++ src/main/java/javareflx/standard_services/messageservice/ServiceMessage.java
@@ -1,13 +1,15 @@
 package javareflx.standard_services.messageservice;
 
+import javafx.util.Pair;
 import javareflx.bri.services.Service;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ServiceMessage extends Service {
 
-    private static HashMap<String, User> messageAccounts;
+    private static HashMap<String, Pair<String, ArrayList<String>>> messageAccounts;
     private String username = null;
 
     public ServiceMessage(){
@@ -32,3 +34,5 @@ public class ServiceMessage extends Service {
         }
     }
 }
+
+
